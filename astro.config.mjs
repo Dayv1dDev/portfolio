ad75f5195e4272e8ef7ea218a8b416133616fcab
Dayv1d <davidmorecam@gmail.com>
2024-06-11 22:53:45 +0200

    Add i18n to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es']
+  }
 });
