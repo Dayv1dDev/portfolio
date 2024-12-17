10ed009deaa7cf9d4d67437774ed5c7e479112cd
Dayv1d <davidmorecam@gmail.com>
2024-12-17 19:01:46 +0100

    Delete i18n by the moment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'es']
-  }
+  integrations: [tailwind()]
 });
